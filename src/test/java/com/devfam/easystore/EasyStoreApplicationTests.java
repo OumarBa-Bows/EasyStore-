82f1697767b0fd34b2b77cd975cdc00b19f100b6
OumarBa-Bows <58187355+OumarBa-Bows@users.noreply.github.com>
2022-05-07 17:23:01 +0200

    first commit

---
+++ src/test/java/com/devfam/easystore/EasyStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devfam.easystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
